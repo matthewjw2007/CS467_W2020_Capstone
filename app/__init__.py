1bf32b0f735f64201e9c1dd43ef6748f97906a8f
Brian Sprague <brian@Brians-iMac.local>
2020-01-10 11:35:25 -0800

    Adjusted directory structure. Fixed CSS.

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from config import Config
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates', static_folder='./static')
 app.config.from_object(Config)
 
 # Import BP files
