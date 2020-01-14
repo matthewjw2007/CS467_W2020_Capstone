b8a5f85995308e9bc9c22469d5666a8e8ce6ff89
Matthew Esqueda <matthewjw2007@yahoo.com>
2020-01-13 18:02:28 -0800

    updated home.html

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('base.html', title='Home')
+    return render_template('home.html', title='Home')
 
 
 @app.route('/my_pantry')
