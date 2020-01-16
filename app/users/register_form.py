048b1b2b3fa077450e4ca64f5968ea14a9f96183
Brian Sprague <brian@Brians-iMac.local>
2020-01-15 20:25:06 -0800

    Updates to structure and added register/login/logout routes

---
+++ app/users/register_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    verify_password = PasswordField('Verify Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
---
+++ app/users/routes.py
@@ -0,0 +1,90 @@
+from flask import Flask, Blueprint, jsonify, make_response, request, render_template, flash, redirect, url_for
+from flask import current_app as app
+from flask_login import login_user, login_required, logout_user
+import constants
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.users.register_form import RegisterForm
+from app.models import User
+from app import db
+
+bp = Blueprint('users', __name__, template_folder='templates')
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+
+    if request.method == 'GET':
+
+        form = RegisterForm()
+        return render_template('register.html', title='Register', form=form)
+
+    if request.method == 'POST':
+
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Check that the new user isn't already registered
+        user =  User.query.filter_by(email_addr=email).first()
+        if user:
+            flash('A user with this email address already exists.')
+            return redirect(url_for('main.index'))
+
+        else:
+            # Create the new user object using the PBKDF2 with SHA-256 hashing standard with 10,000 iterations and a salt length of 128 bytes
+            new_user = User(username=username, email_addr=email, password=generate_password_hash(password, method='pbkdf2:sha256:10000', salt_length=128))
+
+            # Add the new user to the db
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('main.index'))
+        
+    # All other verbs
+    else:
+        # Make a response object with a JSON body
+        res = make_response(jsonify(error='Invalid HTTP method used for request'))
+        res.status_code = 405
+        return res
+        
+
+# Setup route for /login
+# methods variable defines accepted methods to this route
+# using full list of HTTP verbs from constants.py to allow
+# for error handling if an invalid verb is submitted to this route
+@bp.route('/login', methods=constants.http_verbs)
+# @bp.route('/index', methods=constants.http_verbs)
+def login():
+    # GET
+    if request.method == 'POST':
+
+        username = request.form.get('username')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
+
+        # Fidn the user
+        user =  User.query.filter_by(username=username).first()
+        password_is_correct = check_password_hash(user.password, password)
+
+        # Valid credentials are presented
+        if user and password_is_correct:
+            login_user(user)
+            return redirect(url_for('main.index'))
+
+        # Invalid credentials presented
+        else:
+            flash('Incorrect username and/or password entered. Try again.')
+            return redirect(url_for('main.index'))
+        
+    # All other verbs
+    else:
+
+        # Make a response object with a JSON body
+        res = make_response(jsonify(error='Invalid HTTP method used for request'))
+        res.status_code = 405
+        return res
+
+@bp.route('/logout', methods=constants.http_verbs)
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
