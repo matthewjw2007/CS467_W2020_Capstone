0920836f4ccf0ce00487cfb2b4cf94ad1a9f2fe1
Brian Sprague <brian@Brians-iMac.local>
2020-02-20 13:51:42 -0800

    Pantry route built out

--- app/pantry/routes.py
+++ app/pantry/routes.py
@@ -6,11 +6,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app.recipes.search_form import SearchForm
 from app.models import User, Pantry
 from app import db
-<<<<<<< HEAD
-=======
 from app.scraper.scraper import recipe_search
 from app.scraper.all_recipes import get_recipe
->>>>>>> 6d326da6168f961d10c8eb684d7f50b54b1ba88b
 
 bp = Blueprint('pantry', __name__, template_folder='templates')
 
@@ -19,15 +16,40 @@ bp = Blueprint('pantry', __name__, template_folder='templates')
 @login_required
 def view_pantry():
     if request.method == 'POST':
-        pantry_items = request.form.get('pantry_items')
-        payload = pantry_items
+        payload = dict()
+        pantry_items = list()
+        print ('Printing form data now!')
+        print (request.json)
+        for items in request.json:
+            pantry_items.append(items['value'])
+            print (pantry_items)
+        for item in pantry_items:
+            new_item = Pantry(owner=current_user.id, name=item, quantity=1, units_used='ounces')
+            db.session.add(new_item)
+            db.session.commit()
+        payload['message'] = 'Successfully added items!'
         return render_template('pantry_list.html', payload=payload)
     if request.method == 'GET':
         payload = dict()
-        pantry_items = Pantry.query.filter_by(owner=current_user.id).first()
+        pantry_items = Pantry.query.filter_by(owner=current_user.id).all()
         if pantry_items:
             payload['pantry'] = pantry_items
         else:
             payload['message'] = "You don't have any items in your pantry. Would you like to add some?"
 
         return render_template('pantry_list.html', payload=payload)
+
+@bp.route('/<item_id>', methods=constants.http_verbs)
+@login_required
+def delete_item(item_id):
+    if request.method == 'DELETE':
+        # Find the user
+        user = User.query.filter_by(id=current_user.id).first()
+        item = Pantry.query.filter_by(id=item_id).first()
+
+        if item.owner == user.id:
+            db.session.delete(item)
+            db.session.commit()
+            return "Successful deletion."
+        else:
+            return "Unable to delete this recipe."
