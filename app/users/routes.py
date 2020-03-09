e87e2271a6f17934ba6beb7a017b220489111adf
Matthew Esqueda <matthewjw2007@yahoo.com>
2020-03-09 10:19:36 -0700

    small style changes

--- app/users/routes.py
+++ app/users/routes.py
@@ -236,7 +236,7 @@ def get_recipes():
         # Find the user
         user = User.query.filter_by(id=current_user.id).first()
         recipes = Recipes.query.filter_by(added_by=user.id).all()
-        # print(f"recipes = {recipes}")
+        print(f"recipes = {recipes}")
         return render_template('my_recipes.html', title='My Recipes', recipes=recipes)
 
 
