d99e92091f2affe097a6baea333022bdcd2b0c12
Matthew Esqueda <matthewjw2007@yahoo.com>
2020-03-11 17:59:44 -0700

    heroku deployment

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = create_app()
 # Enables running the app locally.
 # Access through http://127.0.0.1:8080 or http://localhost:8080
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='8080', debug=True)
+    app.run(host='127.0.0.1', port='8080', debug=False)
